ba637de8df17d542e3941926a0c3dce0d85e4afb
Alex Cani <alexcani109@gmail.com>
2023-12-20 14:36:46 -0300

    Read lines as vec function added to library

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,8 @@ pub fn read_lines(filepath: &str) -> io::Result<io::Lines<io::BufReader<File>>> 
     let file = File::open(filepath)?;
     Ok(io::BufReader::new(file).lines())
 }
+
+pub fn read_lines_as_vec(filepath: &str) -> io::Result<Vec<String>> {
+    let lines = read_lines(filepath)?;
+    Ok(lines.flatten().collect())
+}
